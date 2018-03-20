caf0682e3a174ccb922b464914b7fa4798f3386c
iceyang <w@icey.cc>
2018-03-20 21:25:51 +0800

    '活动速算结果页完成'

--- dist/js/index.js
+++ dist/js/index.js
@@ -32,27 +32,8 @@ $(function () {
     }
   })
 
-  var mySwiper = new Swiper ('.swiper-container', {
-        direction: 'vertical',
-        loop: true,
 
-        // 如果需要分页器
-        pagination: {
-          el: '.swiper-pagination',
-        },
 
-        // 如果需要前进后退按钮
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-
-        // 如果需要滚动条
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
-      })
-      </script>
 })
 
 // 1 screen1中点击 跳转下一模块
